173e45113a4ad7254e1ba556a682b7f4b4d6964b
מחשב נייד X <@>
2026-03-01 14:53:20 +0200

    first commit

---
+++ src/main/java/yeonatano/steganography_system/SteganographySystemApplication.java
@@ -0,0 +1,13 @@
+package yeonatano.steganography_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteganographySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteganographySystemApplication.class, args);
+	}
+
+}
